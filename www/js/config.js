94c9fedad9b81f565d0dfbcb92d651a9ae2fb407
pablo_diaz <pablo_diaz@avansys.com.mx>
2018-02-20 19:14:42 -0600

    commit

--- www/js/config.js
+++ www/js/config.js
@@ -1,3 +1,3 @@
-var ruta_generica = "http://172.16.1.30:8000";
-//var ruta_generica = "http://autosoft2.avansys.com.mx";
+//var ruta_generica = "http://172.16.1.30:8000";
+var ruta_generica = "http://autosoft2.avansys.com.mx";
 
