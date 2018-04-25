ecb5c1d24ef5a3f54e7961e8859bbec2e4451946
SistemasAvanzadosIntegrales <apps_xdk@avansys.com.mx>
2018-04-25 11:19:16 -0500

    Update sync.js

--- www/js/sync.js
+++ www/js/sync.js
@@ -1,65 +1,36 @@
-var sync = {
-    save_local_data: function(data){
-        alert('save local data');
-        this.save_clients(data.clients);
-        this.save_vehicles(data.vehicles);
-        this.save_techs(data.techs);
-        this.save_cataloge(data.cataloge);
-    },
-    save_clients : function(clients){
+// Wait for Cordova to load
+//
+document.addEventListener("deviceready", onDeviceReady, false);
 
-    },
-    save_vehicles : function(vehicles){
-
-    },
-    save_techs : function(techs){
-
-    },
-    save_cataloge : function(cataloge){
-
-    },
+// Cordova is ready
+//
+function onDeviceReady() {
+    var db = window.sqlitePlugin.openDatabase({
+      name: 'my.db',
+      location: 'default',
+      androidDatabaseImplementation: 2
+    });
+    db.transaction(populateDB, errorCB, successCB);
 }
 
-function sync_get_data(){
-    if(localStorage.getItem('need_sync_get_data')){
-        let session = JSON.parse(localStorage.getItem('session'));
-        $.ajax({
-            url: ruta_generica+"/api/v1/sync_get_data",
-            type: 'POST',
-            dataType: 'JSON',
-            data: {
-                token: session.token,
-            },
-            success:function(resp) {
-                sync.save_local_data(resp);
-            }
-        });
-        localStorage.setItem('need_sync_get_data', true);
-    }
+// Populate the database
+//
+function populateDB(tx) {
+    tx.executeSql('DROP TABLE IF EXISTS DEMO');
+    tx.executeSql('CREATE TABLE IF NOT EXISTS DEMO (id unique, data)');
+    tx.executeSql('INSERT INTO DEMO (id, data) VALUES (1, "First row")');
+    tx.executeSql('INSERT INTO DEMO (id, data) VALUES (2, "Second row")');
 }
 
+// Transaction error callback
+//
+function errorCB(tx, err) {
+    alert("Error processing SQL: "+err);
+    console.log(err);
+}
 
-
-document.addEventListener("deviceready", function(){
-    var db = window.sqlitePlugin.openDatabase({name: "my.db"});
-    alert(JSON.stringify(db));
-    db.transaction(function(tx) {
-    tx.executeSql('DROP TABLE IF EXISTS test_table');
-    tx.executeSql('CREATE TABLE IF NOT EXISTS test_table (id integer primary key, data text, data_num integer)');
-
-    tx.executeSql("INSERT INTO test_table (data, data_num) VALUES (?,?)", ["test", 100], function(tx, res) {
-      alert("insertId: " + res.insertId + " -- probably 1");
-      alert("rowsAffected: " + res.rowsAffected + " -- should be 1");
-
-      db.transaction(function(tx) {
-        tx.executeSql("select count(id) as cnt from test_table;", [], function(tx, res) {
-          alert("res.rows.length: " + res.rows.length + " -- should be 1");
-          alert("res.rows.item(0).cnt: " + res.rows.item(0).cnt + " -- should be 1");
-        });
-      });
-
-    }, function(e) {
-      alert("ERROR: " + e.message);
-    });
-    });
-}, false);
+// Transaction success callback
+//
+function successCB() {
+    alert("success!");
+}
