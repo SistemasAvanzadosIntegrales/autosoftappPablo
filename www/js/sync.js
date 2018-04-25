9afbe007ad211b02c91907f4aa821bea8c85cfb5
SistemasAvanzadosIntegrales <apps_xdk@avansys.com.mx>
2018-04-25 11:52:34 -0500

    Update sync.js

--- www/js/sync.js
+++ www/js/sync.js
@@ -1,5 +1,5 @@
 function sync(data){
-    console.log(data);
+    alert(device.platform);
     var db = window.sqlitePlugin.openDatabase({
         name: 'my.db',
         location: 'default',
@@ -10,14 +10,22 @@ function sync(data){
         tx.executeSql('CREATE TABLE IF NOT EXISTS techs (id, name)');
         for(let i = 0; i < data.techs.length; i++)
         {
-            alert(data.techs[i].name);
+            tx.executeSql('INSERT INTO  VALUES (?,?)', [data.techs[i].id, data.techs[i].name]);
         }
 
     }, function(error) {
-        console.log('Transaction ERROR: ' + error.message);
+        alert('Transaction ERROR: ' + error.message);
     }, function() {
-        console.log('Populated database OK');
+        alert('Populated database OK');
     }).bind(data);
+
+    db.transaction(function(tx) {
+        tx.executeSql('SELECT count(*) AS tecnicos FROM techs', [], function(tx, rs) {
+            alert('Record count (expected to be 2): ' + rs.rows.item(0).mycount);
+        }, function(tx, error) {
+            alert('SELECT error: ' + error.message);
+        });
+    });
 }
 function get_data(){
     if(localStorage.getItem('need_sync_get_data')){
