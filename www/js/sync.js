9c78441e4d28bffcdc823bf6a8de91711d7c30e2
SistemasAvanzadosIntegrales <apps_xdk@avansys.com.mx>
2018-06-07 16:22:02 -0500

    Update sync.js

--- www/js/sync.js
+++ www/js/sync.js
@@ -69,10 +69,17 @@ function __make_db(data, call_back_function = null){
             var vehicle_inspections = inspection.vehicle_inspections;
             for(var x = 0; x < vehicle_inspections.length; x++)
             {
-                var point = vehicle_inspections[x];
-                var files = JSON.stringify(point.files);
-                var sql2 = "INSERT INTO vehicle_inspections (id, inspection_id, point_id, price, severity, status, cataloge, category, origen, files) VALUES ("+point.id+", "+point.inspections_id+", "+point.inspection_id+", '"+point.price+"', "+point.severity+", "+point.status+", '"+point.catalogue.name+"', '"+point.catalogue.inspection.name+"', 'server', '"+files+"' )";
-                tx.executeSql(sql2);
+                try {
+                    if (!vehicle_inspections[x].catalogue.inspection) {
+                        continue;
+                    }
+                    var point = vehicle_inspections[x];
+                    var files = JSON.stringify(point.files);
+                    var sql2 = "INSERT INTO vehicle_inspections (id, inspection_id, point_id, price, severity, status, cataloge, category, origen, files) VALUES ("+point.id+", "+point.inspections_id+", "+point.inspection_id+", '"+point.price+"', "+point.severity+", "+point.status+", '"+point.catalogue.name+"', '"+point.catalogue.inspection.name+"', 'server', '"+files+"' )";
+                    tx.executeSql(sql2);
+                } catch (e) {
+                    console.log(e);
+                }
             }
         }
     }, function(error) {
